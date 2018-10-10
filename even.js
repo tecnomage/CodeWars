b3292c71b01206a8664094b0026e71b57040adbd
tecnomage <tecnocratay@gmail.com>
2018-10-10 19:05:06 -0300

    esta retornando a posicao corretamente

--- even.js
+++ even.js
@@ -17,32 +17,20 @@ var func = function iqTest(numbers){
     // 2 array é par
     // 1 array é impar
     var somaNumeros = num.reduce((acc,atual) => acc+=atual)>1?2:1; 
-    
-    var posicao = somaNumeros>1? numbers.indexOf()
-    
- //filtrar se é 0 ou se é 1
-// pega a posicao +1 e imprime 
-// se for 
-
-    //LOGICA DE EXECUCAO
-    //var paridade = num.reduce((acc,atual) => acc+=atual) % 2
-     
-    //console.log(cada.indexOf(num)+1);
-    
-    //console.log(paridade);
-    //console.log(num)
-    
+    console.log("soma numeros eh " + somaNumeros)
+    //filtrar se é 0 ou se é 1
+   // pega a posicao +1 e imprime 
+   // se for 
+   // 1 1 1 1 0
+   console.log(num)
+    var posicao = somaNumeros>1? num.indexOf(0)+1:num.indexOf(1)+1;
+    console.log("posicao eh " + posicao)
+    //console.log(posicao);
     
 }
 
+func("1 2 8 4 12 10")
 func("2 4 7 8 10");
 func ("3 5 7 9 2");
 
-// function iqTest(numbers){
-//     var cada = numbers.map(x => x% 2);
-//     var paridade = numbers.reduce((acc,atual) => acc+=atual) % 2
-//     var num = cada.filter(x => x!=paridade);
-    
-//    return cada.indexOf(num)+1;
-    
-// }
+
