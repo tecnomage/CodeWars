17e47994c58fc7488fcdb1977312cfa0140ee9da
tecnomage <tecnocratay@gmail.com>
2018-10-17 16:32:09 -0300

    subArray alternatives

--- subarraySum.js
+++ subarraySum.js
@@ -5,11 +5,47 @@
 
 //https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/javascript
 var maxSequence = function(arr){
-  arr.length 
+  var currentSum = 0;
+  return arr.reduce(function(maxSum, number){
+      currentSum = Math.max(currentSum+number, 0);
+      return Math.max(currentSum, maxSum);
+  }, 0);
+
 }
 
 
 //The maximum sum subarray problem consists 
 //in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 //(maxSequence([]), 0
-//maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6
+//maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6  [4, -1, 2, 1]
+
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); 
+
+
+// var maxSequence = function(arr){
+//   var rec = 0, sum = 0;
+
+//   for (var i = 0; i < arr.length; i++) {
+//     sum = 0;
+//     for (var j = i; j < arr.length; j++) {
+//       sum += arr[j];
+//       if (sum > rec) {
+//         rec = sum;      
+//       }
+//     }
+//   }
+  
+//   return rec;
+// }
+
+
+
+// var maxSequence = function(arr){
+//   var min = 0, ans = 0, i, sum = 0;
+//   for (i = 0; i < arr.length; ++i) {
+//     sum += arr[i];
+//     min = Math.min(sum, min);
+//     ans = Math.max(ans, sum - min);
+//   }
+//   return ans;
+// }
