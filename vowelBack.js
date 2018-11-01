d9db51e3c7cbc024351e304743d62ea8250535df
tecnomage <tecnocratay@gmail.com>
2018-11-01 17:44:13 -0300

    vowel

--- vowelBack.js
+++ vowelBack.js
@@ -1,7 +1,54 @@
-function vowelBack(s){
+(function vowelBack(s) {
+
+    var str=[];
+    var tamanho = s.length
+
+    //novoArray.toLowerCase();
+    //consoantes +9
+    
+    //TODO verificar se o resultado da soma esta dentro do alfabeto ou algo do tipo
+
+    for (let i=0; i <= s.length - 1; i++) {
+        var cod,char;
+        char=s.charAt(i).toLowerCase();
+        //i>0 cod esta retornando NaN
+        cod = char.charCodeAt(i)
+        if (char.charCodeAt(i) === 99 || char.charCodeAt(i) === 111)
+            cod - 1
+        if (char.charCodeAt(i) === 100)
+            cod  - 3
+
+        if (char.charCodeAt(i) === 97 || 101 || 105 || 111 || 117) {
+            cod + 9
+            if (cod === 99 || 111) {
+                cod - 1
+            } else if (cod === 100) {
+                cod - 3
+            }
+        }
+        else {
+            cod = char.charCodeAt(i) - 5
+        }
+        
+        str.push(String.fromCharCode(cod))
+
+        //     //limite alfabeto tabela  ascii
+        //     // superior 122 (z)
+        //     // inferior 97  (a)
+    }
 
 
-}
+    function foraDoAlfabeto(n){
+        var fora= n < 97 || n > 122? true : false;
+        return fora;
+    }
+
+    // var char = s[0]
+    // console.log(char.charCodeAt(0));
+    // var char2 = char.charCodeAt(0) + 10
+    // console.log(char2);
+    console.log(str)
+})("aa")
 
 
 //"\n".charCodeAt(0);
