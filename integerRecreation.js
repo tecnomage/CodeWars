ad0f5d4885c7cf3411a98379d0725203161e9687
tecnomage <tecnocratay@gmail.com>
2018-11-08 14:59:37 -0200

    metade do caminho

--- integerRecreation.js
+++ integerRecreation.js
@@ -1,16 +1,28 @@
 //Math.pow(7, 3)
+//Math.sqrt(n)
+// we want to find all integers between
+// m and n whose sum of
+// squared divisors is itself a square. 
+
 (function listSquared(m, n) {
-    var lista=[];
-    var soma=0;
-    for (let i = m; i <= n; i++){
-        if(n%i===0){
-            let quadrado = Math.pow(i,2)
+    var lista = [];
+    var soma = 0;
+    var saida = [] ;
+    for (let i = m; i <= n; i++) {
+        if (n % i === 0) {
+            let raiz;
+            let quadrado = Math.pow(i, 2)
             lista.push(quadrado);
-            soma+=quadrado;
+            soma += quadrado;
+
+            raiz = Math.sqrt(soma);
+            if(raiz!==NaN && Number.isInteger(raiz) )
+            saida.push(Math.sqrt(soma));
         }
-        
+
     }
     console.log(soma)
+    console.log(saida)
     //TODO parsear a lista para verificar os roots
 
-})(1,42)
+})(42, 250)
