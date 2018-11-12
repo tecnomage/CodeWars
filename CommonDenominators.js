ecce6700a3086bc46cd52aa41d4698991fdea7b1
tecnomage <tecnocratay@gmail.com>
2018-11-12 20:17:49 -0200

    denominador quase ok

--- CommonDenominators.js
+++ CommonDenominators.js
@@ -1,22 +1,28 @@
 (function convertFrac(lst) {
-    var denominadores, numeradores;
-    //let LCD = lcd.bind(this);
-    denominadores = lst.map(x => x[1])
-    var deno = denominadores.reduce((acc, atual) => acc *= atual)
-    console.log(deno)
+  var denominadores;
+  let numeradores, deno, saida = [];
+  //let LCD = lcd.bind(this);
+  denominadores = lst.map(x => x[1])
+  deno = denominadores.reduce((acc, atual) => acc *= atual)
+  saida = lcd(deno)
+  console.log(saida)
 })([[1, 2], [1, 3], [1, 4]])
 
 function lcd(arr) {
-    var divisor = [];
-    var n = 2;
-    while (deno > 0) {
-        if (deno % n == 0) {
-            divisor.push(n)
-        }
-
-
+  var divisor = [];
+  var deno;
+  var n = 2
+  while (arr > n) {
+    if (arr % n == 0) {
+      divisor.push(n)
     }
-
+    else{
+      n++;
+      divisor.push(n)
+    }
+    arr = arr / n
+  }
+  return divisor;
 }
 
 
