e3ba62d73e70b9bb00db89d0ba4c02e7f9535fd3
tecnomage <tecnocratay@gmail.com>
2018-11-12 19:12:57 -0200

    merge

---
+++ CommonDenominators.js
@@ -0,0 +1,10 @@
+///https://www.codewars.com/kata/common-denominators
+function convertFrac(lst){
+    //Your code here
+  }
+
+
+//convertFracs [(1, 2), (1, 3), (1, 4)] `shouldBe` [(6, 12), (4, 12), (3, 12)]
+
+
+
