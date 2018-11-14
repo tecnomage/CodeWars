53d3ae3ca03707b006978e0caa7ffd1a81b178ab
tecnomage <tecnocratay@gmail.com>
2018-11-14 15:30:18 -0200

    CDeno evoluindo.

--- CDeno.js
+++ CDeno.js
@@ -3,35 +3,15 @@
   //let LCD = lcd.bind(this);
   denominadores = lst.map(x => x[1])
   var deno = denominadores.reduce((acc, atual) => acc *= atual)
-  //TODO alterar para pegar todos denominadores
-  var teste = lcm(deno,[2,3,4])
-  console.log(deno)
+  
+  var teste = denominadores.reduce(lcm)
   console.log(teste)
+
+  
 })([[1, 2], [1, 3], [1, 4]])
 
-//TODO alterar para aceitar mais que 2 parametros
-function lcm(soma,...n) {
-    
-  if (typeof n !== 'object')
-    return false;
-  return (!soma) ? 0 :
-    Math.abs((soma) / gcd(n));
-}
 
-//TODO alterar para aceitar mais que 2 parametros
-function gcd(...args) {
-  x = Math.abs(x);
-  y = Math.abs(y);
-  var div = args.reduce((acc,n)=>{
-    while
-  })
- 
-  return x;
-}
+function gcd(a, b){ a ? gcd(b % a, a) : b;}
 
-  //  while (y) {
-  //     var t = y;
-  //     y = x % y;
-  //     x = t;
-   
-  // }
+function lcm(a, b) {a * b / gcd(a, b);}
+
