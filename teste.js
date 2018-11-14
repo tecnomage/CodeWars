beed6c0c0ef818d6e5704baebe28fe486bba2686
tecnomage <tecnocratay@gmail.com>
2018-11-14 16:44:05 -0200

    teste (reduce)

--- teste.js
+++ teste.js
@@ -1,5 +1,14 @@
-var teste=2222
+var teste=[2,3,1]
 
-if(teste){
-    console.log('entrou')
+// if(teste){
+//     console.log('entrou')
+// }
+
+console.log(teste.reduce(soma));
+
+
+
+function soma(a,b){
+    
+    return a+b
 }
