d5883e11b2700281f6948459978eb75fda2c661d
tecnomage <tecnocratay@gmail.com>
2018-11-21 19:28:03 -0200

    charcount

--- absSum.js
+++ absSum.js
@@ -1,9 +1,6 @@
 (function getAbsSum(arr) {
 
-    var saida = arr.length === 1 ? Math.abs(arr[0]) : arr.reduce((acc, n) => acc += Math.abs(n))
-    var saida2 = arr.reduce((acc, n) => acc += Math.abs(n))
-    //return arr.reduce((acc, n) => acc += Math.abs(n))
-    //console.log(Math.abs(arr[0]))
-    console.log(saida2)
+    var saida = arr.length === 1 ? Math.abs(arr[0]) : arr.reduce((acc, n) => acc = Math.abs(acc) + Math.abs(n))
+    console.log(saida)
     return saida
-})([-1, -3, -5, -4, -10, 0])
+})([-1, -3])
---
+++ charCount.js
@@ -0,0 +1,4 @@
+//TODO https://edabit.com/challenge/kbFhwaDyrd79JrgeB
+function charCount(myChar, str) {
+	
+}
