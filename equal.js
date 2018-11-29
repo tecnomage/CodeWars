28a978399e7cc5d29ec61f2794d58f3e3f1ee7df
tecnomage <tecnocratay@gmail.com>
2018-11-29 19:30:38 -0200

    equal finalizado

--- equal.js
+++ equal.js
@@ -1,10 +1,18 @@
-function equal(a, b, c) {
-    let count;
-    for (let index = 0; index < arguments.length; index++) {
-        if(a==b || a==c || b==c || b==)
-     }
+(function equal(a, b, c) {
+    let set = new Set([a, b, c]);
+    let count = arguments.length - set.size;
 
-    
+    console.log(set.size);
+    console.log(count);
+    if (count == 0) {
+        return 0
+    }
+    else if (count == 1) {
+        return 2
 
+    }else if (count == 2){
+        return 3
+    }
 
-     https://edabit.com/challenge/PKiTjA7SLK99ZZ8GK
+})(1, 1, 3)
+     //https://edabit.com/challenge/PKiTjA7SLK99ZZ8GK
