259ff22af957990ffeafb31e6ec41b08321531ae
tecnomage <tecnocratay@gmail.com>
2018-12-05 18:40:02 -0200

    missingNums

---
+++ missingNums.js
@@ -0,0 +1,7 @@
+
+
+function missingNums(arr) {
+	
+}
+
+//https://edabit.com/challenge/rgdT9sZ4QhsKhaSw9
--- scretch.js
+++ scretch.js
@@ -1,5 +1,25 @@
 // JavaScript
-function stretchTruth(num,str,arr) {
-}
+(function stretchTruth(num, str, arr) {
+    let countN = 0, countS = 0;
+    
+    arr.map(n => {
+        if (n === num) countN++
+        else
+            if (n === str ) countS++;
+
+    })
+    return countN + countS + 1;
+
+})((1, '1', []))
+
+
 
 //Sometimes We Stretch the Truth
+
+
+//Test.assertEquals(stretchTruth(1, 'Sometimes We Stretch the Truth', ['1','Sometimes We Stretch the Truth']), 2)
+//Test.assertEquals(stretchTruth(1, '1', [2,3]), 1)
+
+//verifica o tipo de n
+//compara com o tipo correto
+//num ou str? soma ao count de cada
