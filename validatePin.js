36868fc554548b7754513a3152949f495ae2b0c2
tecnomage <tecnocratay@gmail.com>
2018-12-06 17:50:32 -0200

    está pegando 4 digitos

--- validatePin.js
+++ validatePin.js
@@ -1,6 +1,7 @@
 (function validatePIN(pin) {
     //let Regex = new Regex(')
-   let saida = /d{4}|\d{6}/.test(pin);
+   //let saida = /(\d{4})|(\d{6})/.test(pin);
+   let saida = /^\d{4}$/.test(pin);
    console.log(saida) 
    return false;
-})("123433344444")
+})("1234")
