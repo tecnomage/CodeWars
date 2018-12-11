ad781d895d08e7aefcdf6f77201669bbe5d3215a
tecnomage <tecnocratay@gmail.com>
2018-12-11 17:04:10 -0200

    wurstIsBetter

--- countOnesBinary.js
+++ countOnesBinary.js
@@ -1,16 +1,13 @@
 (function countOnes(i) {
     //filtra pelos 1
     let bin = []
-
     while (i > 0) {
-
         let resto = i % 2;
         bin.push(resto)
-        i = i / 2
-
+        i = Math.floor(i / 2)
     }
 
-    bin.filter(n => n > 0);
+    bin= bin.filter(n => n > 0);
     return bin.length
 })(100)
 
