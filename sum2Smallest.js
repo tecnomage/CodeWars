856f9e87ea17bcab8fb3afff18226fcabdd8dad3
tecnomage <tecnocratay@gmail.com>
2018-12-11 18:07:47 -0200

    merge

--- sum2Smallest.js
+++ sum2Smallest.js
@@ -2,6 +2,8 @@
     
     let valor = arr.sort((v,v2) => v-v2 );
     console.log(valor)
+    console.log(valor[0] + valor[1])
    return valor[0] + valor[1]
-    
-})([2,5,6,7,8,9,10])
+    //8
+})([ -1, 2, 6, 9 ])
+//https://edabit.com/challenge/GNgCfKHWfQwByBNqa
