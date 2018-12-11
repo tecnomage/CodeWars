f541a699cbd3c4d046a108f96e0cfa27cff021e4
tecnomage <tecnocratay@gmail.com>
2018-12-11 18:30:21 -0200

    wurst is ok

--- wurstIsBetter.js
+++ wurstIsBetter.js
@@ -1,6 +1,20 @@
 function wurstIsBetter(str) {
-    //usar str replace
-    // /[]/
+    const sausages = ['Kielbasa', 'Chorizo', 'Moronga', 'Salami', 'Sausage', 'Andouille', 'Naem', 'Merguez', 'Gurka', 'Snorkers', 'Pepperoni']
+    const re = new RegExp(sausages.join('|'), 'gi');
+    return str.replace(re, 'Wurst');
 
 }
 //https://edabit.com/challenge/j7s4LTKexAEazQApv
+
+
+// Kielbasa
+// Chorizo
+// Moronga
+// Salami
+// Sausage
+// Andouille
+// Naem
+// Merguez
+// Gurka
+// Snorkers
+// Pepperoni
