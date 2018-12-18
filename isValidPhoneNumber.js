4cacd0c3e1478950b7a9b3b4d343424a4d0ec731
tecnomage <tecnocratay@gmail.com>
2018-12-18 14:16:45 -0200

    isvalid

--- isValidPhoneNumber.js
+++ isValidPhoneNumber.js
@@ -1,3 +1,11 @@
-function isValidPhoneNumber(str) {
-	
+const regex = function isValidPhoneNumber(str) {
+    let expre = new RegExp('[(]?\d{3}[)]?\s?\d{3}\s?-?\s?\d{4}','g')
+    //let expre = new RegExp('[(]?\d{3}[)]?\s?-?\s?\d{3}\s?-?\s?\d{4}','g')
+    console.log(expre.test(str))
+    return expre.test(str)
 }
+
+
+regex("(123) 456-7890")
+
+//https://edabit.com/challenge/nHaKL55KwW3XaGrAw
