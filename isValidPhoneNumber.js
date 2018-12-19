c670863fa45d44589f5563e7bec2c6ecf37e8b78
tecnomage <tecnocratay@gmail.com>
2018-12-19 17:07:39 -0200

    isValidPhone

--- isValidPhoneNumber.js
+++ isValidPhoneNumber.js
@@ -1,5 +1,5 @@
 const regex = function isValidPhoneNumber(str) {
-    let expre = new RegExp('[(]?\d{3}[)]?\s?\d{3}\s?-?\s?\d{4}','g')
+    let expre = new RegExp('^\(?\d{3})?\s\d{3}\s?-?\s?\d{4}','g')
     //let expre = new RegExp('[(]?\d{3}[)]?\s?-?\s?\d{3}\s?-?\s?\d{4}','g')
     console.log(expre.test(str))
     return expre.test(str)
@@ -8,4 +8,11 @@ const regex = function isValidPhoneNumber(str) {
 
 regex("(123) 456-7890")
 
+// function isValidPhoneNumber(str) {
+// 	return /^\(\d{3}\) \d{3}-\d{4}$/.test(str);
+// }
+
+// const isValidPhoneNumber = str => /^\(\d{3}\) \d{3}-\d{4}$/.test(str)
+
 //https://edabit.com/challenge/nHaKL55KwW3XaGrAw
+
